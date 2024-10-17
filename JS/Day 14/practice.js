5303c6ce8de2fdf1a9a17844ed76c7cb45ed19cc
Zirwa Abid <zirwaabid112@gmail.com>
2024-10-17 11:46:40 -0700

    delta

---
+++ JS/Day 14/practice.js
@@ -0,0 +1,13 @@
+let firstName = "Zirwa";
+let LastName = "Abid";
+let fullName = firstName + " " + LastName;
+console.log(fullName)
+
+let a = 3;
+let b = a++;
+console.log(b); //3
+console.log(a);//4
+
+console.log(firstName.length)
+console.log(firstName[firstName.length-1]) //a
+
