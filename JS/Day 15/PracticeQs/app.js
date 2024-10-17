716edefa52ef7ee99e9024c7218f739e59d2f1d3
Zirwa Abid <zirwaabid112@gmail.com>
2024-10-17 13:41:21 -0700

    delta

--- JS/Day 15/PracticeQs/app.js
+++ JS/Day 15/PracticeQs/app.js
@@ -1,39 +1,55 @@
-// let pencilPrice = 10;
-// let eraserPrice = 15;
-// let output = `The total Price is: ${pencilPrice + eraserPrice} Rupees.`
-// console.log(output);
+let pencilPrice = 10;
+let eraserPrice = 15;
+let output = `The total Price is: ${pencilPrice + eraserPrice} Rupees.`
+console.log(output);
 
-// let a = 3;
-// let b = 3;
-// console.log(a = b)
+let a = 3;
+let b = 3;
+console.log(a = b)
 
-// // practice question
-// let trafficLights = "green";
-// if (trafficLights === "red") {
-//     console.log("stop");
-// } else if (trafficLights === "yellow") {
-//     console.log("slow");
-// } else if (trafficLights === "green") {
-//     console.log("you can go");
-// }
+// practice question
+let trafficLights = "green";
+if (trafficLights === "red") {
+    console.log("stop");
+} else if (trafficLights === "yellow") {
+    console.log("slow");
+} else if (trafficLights === "green") {
+    console.log("you can go");
+}
 
-// // practice question
-// let size = "M";
-// if (size === "XL") {
-//     console.log("Price of PopCorn is:250 Rupees")
-// } else if (size === "L") {
-//     console.log("Price of PopCorn is:200 Rupees")
-// } else if (size === "M") {
-//     console.log("Price of PopCorn is:100 Rupees")
-// } else {
-//     console.log("Price of PopCorn is:50 Rupees")
-// }
+// practice question
+let size = "M";
+if (size === "XL") {
+    console.log("Price of PopCorn is:250 Rupees")
+} else if (size === "L") {
+    console.log("Price of PopCorn is:200 Rupees")
+} else if (size === "M") {
+    console.log("Price of PopCorn is:100 Rupees")
+} else {
+    console.log("Price of PopCorn is:50 Rupees")
+}
 
 //practice Question
-let string="ae";
-if (string[0]==="a" && string.length>3) {
+let string = "ae";
+if (string[0] === "a" && string.length > 3) {
     console.log("Good String")
 }
-else{
+else {
     console.log("string is not good")
+}
+
+//Switch statements
+let color = "white";
+switch (color) {
+    case "red":
+        console.log("Stop");
+        break
+    case "yellow":
+        console.log("slow down");
+        break
+    case "green":
+        console.log("you can go");
+        break
+    default:
+        console.log("light is broken");
 }
