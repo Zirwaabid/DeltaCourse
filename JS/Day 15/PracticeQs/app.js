7c058701e282fb847a76a1bbbb31d2b77cf3ec10
Zirwa Abid <zirwaabid112@gmail.com>
2024-10-17 13:26:25 -0700

    delta

---
+++ JS/Day 15/PracticeQs/app.js
@@ -0,0 +1,39 @@
+// let pencilPrice = 10;
+// let eraserPrice = 15;
+// let output = `The total Price is: ${pencilPrice + eraserPrice} Rupees.`
+// console.log(output);
+
+// let a = 3;
+// let b = 3;
+// console.log(a = b)
+
+// // practice question
+// let trafficLights = "green";
+// if (trafficLights === "red") {
+//     console.log("stop");
+// } else if (trafficLights === "yellow") {
+//     console.log("slow");
+// } else if (trafficLights === "green") {
+//     console.log("you can go");
+// }
+
+// // practice question
+// let size = "M";
+// if (size === "XL") {
+//     console.log("Price of PopCorn is:250 Rupees")
+// } else if (size === "L") {
+//     console.log("Price of PopCorn is:200 Rupees")
+// } else if (size === "M") {
+//     console.log("Price of PopCorn is:100 Rupees")
+// } else {
+//     console.log("Price of PopCorn is:50 Rupees")
+// }
+
+//practice Question
+let string="ae";
+if (string[0]==="a" && string.length>3) {
+    console.log("Good String")
+}
+else{
+    console.log("string is not good")
+}
