c22d98b6519c8be4001d2ca9c1d665680dc3d545
Zirwa Abid <zirwaabid112@gmail.com>
2024-10-17 19:57:29 -0700

    delta

---
+++ JS/Day 15/PracticeQs/app2.js
@@ -0,0 +1,35 @@
+let day = 9;
+switch (day) {
+    case 1:
+        console.log("Monday")
+        break;
+    case 2:
+        console.log("Tuesday")
+        break;
+    case 3:
+        console.log("Wednesday")
+        break;
+    case 4:
+        console.log("Thursday")
+        break;
+    case 5:
+        console.log("Friday")
+        break;
+    case 6:
+        console.log("Saturday")
+        break;
+    case 7:
+        console.log("Sunday")
+        break;
+    default:
+        console.log("Not a day number")
+        break;
+}
+
+console.error("something is wrong");
+console.warn("Dont try again");
+let firstName=prompt("Enter Your FirstName");
+let lastName=prompt("Enter Your lastName");
+console.log("Welcome",firstName, lastName, "!")
+let msg=("Welcome "+ firstName +" "+ lastName + "!")
+alert(msg)
