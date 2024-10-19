db6bd3cfc61c82f095e46b98f395181c106825a0
Zirwa Abid <zirwaabid112@gmail.com>
2024-10-19 12:28:58 -0700

    delta

---
+++ JS/Day 16/Assignment/app.js
@@ -0,0 +1,27 @@
+// // question 01
+// let num = [7, 9, 0, -2];
+// let n = 3;
+// let ans = num.slice(0, n)
+// console.log(ans)
+
+// // question 02
+// let num2 = [7, 9, 0, -2];
+// let n2 = 4;
+// let ans2 = num2.slice(1, n2)
+// console.log(ans2)
+
+// // question 03
+let str = prompt("Enter string");
+if (str.length === 0) {
+    console.log("Your string is an empty")
+} else {
+    console.log("Your string is not an empty")
+}
+
+// // question 04
+// let charac = ["a", "b", "c"];
+// if (condition) {
+
+// } else {
+
+// }
