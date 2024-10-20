3bc1ac84d0863df79b00068afc1b14c54fca21e7
Zirwa Abid <zirwaabid112@gmail.com>
2024-10-19 20:47:24 -0700

    delta

---
+++ JS/Day 17/practice/app1.js
@@ -0,0 +1,17 @@
+// print odd numbers
+for (let index = 1; index <= 15; index = index + 2) {
+    console.log(index);
+};
+//print odd numbers in reverse
+for (let index = 15; index >= 1; index = index - 2) {
+    console.log(index);
+};
+
+//print even numbers
+for (let index = 2; index <= 10; index = index + 2) {
+    console.log(index);
+}
+// //print even numbers in reverse
+for (let index = 10; index >= 2; index = index - 2) {
+    console.log(index);
+}
