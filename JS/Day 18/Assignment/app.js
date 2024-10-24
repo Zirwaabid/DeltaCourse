2bd510ca2a6ca61c1c4d7ff8da22686c5113d5a5
Zirwa Abid <zirwaabid112@gmail.com>
2024-10-24 10:40:51 -0700

    delta

---
+++ JS/Day 18/Assignment/app.js
@@ -0,0 +1,21 @@
+// question 01
+let diceRoll = Math.floor(Math.random() * 6) + 1;
+console.log(diceRoll);
+
+// question 02
+const car = {
+    name: "Lamborgini",
+    model: 2024,
+    color: "Black"
+}
+console.log(car.name);
+
+// question 03
+const person = {
+    name: "Zirwa Abid",
+    age: 19,
+    city: "Jahanian"
+}
+person.city = "New York";
+person.country = "United States";
+console.log(person);
