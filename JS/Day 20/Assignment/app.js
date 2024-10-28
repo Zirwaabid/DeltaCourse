ec857fd8c1577d2cba03887bc9f77df8d073ab2b
Zirwa Abid <zirwaabid112@gmail.com>
2024-10-27 18:33:21 -0700

    delta

--- JS/Day 20/Assignment/app.js
+++ JS/Day 20/Assignment/app.js
@@ -1,11 +1,14 @@
-// const arrAverage=(arr)=>{
-
-// }
-
-let arr = [1, 2, 3, 4];
-let sum = 0;
-for (let i = 0; i < arr.length; i++) {
-    sum = (arr[i] + sum) / arr.length ;
-
+// Question 01
+const arrAverage = (arr) => {
+    let sum = 0;
+    for (const numbers of arr) {
+        sum = sum + numbers
+    }
+    return sum / arr.length
 }
-console.log(sum)
+console.log(arrAverage([1, 2, 3, 4, 5]))
+
+// question 02
+const evenNum = (n) => n % 2 === 0;
+console.log(evenNum(2))
+
