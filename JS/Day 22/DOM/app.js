900d22ab0c341c03f807d223ab77e33a2f3b0ecc
Zirwa Abid <zirwaabid112@gmail.com>
2024-10-30 13:05:45 -0700

    delta

---
+++ JS/Day 22/DOM/app.js
@@ -0,0 +1,6 @@
+let smallImages = document.getElementsByClassName("logo");
+for (let i = 0; i < smallImages.length; i++) {
+    console.dir(smallImages[i]);
+}
+
+console.dir(document.querySelector(".footer a"))
