8278a0145bc0e97437588ec96a8599f098ed9c73
Zirwa Abid <zirwaabid@gmail.com>
2024-11-29 13:13:51 -0800

    delta

---
+++ Backend/Day30/Fruits/index.js
@@ -0,0 +1,6 @@
+let apple = require("./apple");
+let banana = require("./banana");
+let orange = require("./orange");
+
+let fruits = [apple, banana, orange];
+module.exports = fruits;
--- Backend/Day30/node.js
+++ Backend/Day30/node.js
@@ -1,5 +1,9 @@
-//to access other js file in this file
-const math = require("./math.js")
-console.log(math.g)
-console.log(math.sum(1,2))
-console.log(math.pi)
+//to access other js file  in this file
+// const math = require("./math.js")
+// console.log(math.g)
+// console.log(math.sum(1,2))
+// console.log(math.pi)
+
+//to access other folder  in this file
+let info=require("./Fruits");
+console.log(info)
