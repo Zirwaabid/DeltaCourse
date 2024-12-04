72dca8cb19d540b50782688032d40524e6f2f551
Zirwa Abid <zirwaabid@gmail.com>
2024-12-04 12:56:44 +0500

    delta

--- Backend/express/Day31/expressDir/index.js
+++ Backend/express/Day31/expressDir/index.js
@@ -1,3 +1,11 @@
 const express = require("express");
 let app = express();
-console.log(app)
+
+let port = 8080;
+app.listen(port, () => {
+    console.log(`app is listening to port ${port}`)
+});
+
+app.use((req, res) => {
+    console.log("request received")
+})
