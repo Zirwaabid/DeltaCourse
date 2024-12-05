7239869f0a69ea00c79650b5070e7a4890b3fe3f
Zirwa Abid <zirwaabid@gmail.com>
2024-12-05 12:16:44 +0500

    delta

--- Backend/express/Day31/expressDir/index.js
+++ Backend/express/Day31/expressDir/index.js
@@ -6,6 +6,31 @@ app.listen(port, () => {
     console.log(`app is listening to port ${port}`)
 });
 
-app.use((req, res) => {
-    console.log("request received")
+// app.use((req, res) => {
+//     console.log("request received")
+//     // res.send("this is a basic response")
+//     // res.send({
+//     //     name: "apple",
+//     //     color: "red"
+//     // })
+//     let code = "<h1>Fruits</h1><ul><li>Apple</li></ul>"
+//     res.send(code);
+// })
+
+// get req 
+app.get("/", (req, res) => {
+    res.send("contacted root path")
+})
+app.get("/apple", (req, res) => {
+    res.send("you contacted apple path");
+})
+app.get("/orange", (req, res) => {
+    res.send("you contacted orange path");
+})
+app.get("*", (req, res) => {
+    res.send("This path does not exist");
+})
+// post req 
+app.post("/", (req, res) => {
+res.send("You send a post request");
 })
