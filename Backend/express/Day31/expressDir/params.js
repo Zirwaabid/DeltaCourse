4c5623d706871733645ad4b99363e24b5557e154
Zirwa Abid <zirwaabid@gmail.com>
2024-12-06 12:36:59 +0500

    delta

---
+++ Backend/express/Day31/expressDir/params.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+let port = 8080;
+app.listen(port, () => {
+    console.log(`app is listening to port ${port}`)
+});
+
+//response of parameters req
+app.get("/ig/:username/:id", (req, res) => {
+    let { username, id } = req.params;
+    let htmlstr = `<h1>Hi Welcome to @${username} page</h1>`
+    res.send(htmlstr);
+    // res.send(`This accout belongs to @${username} and your id is ${id}`)
+})
+
+//response of queries req
+app.get("/search", (req, res) => {
+    let { q } = req.query;
+    if (!q) {
+        res.send("Nothing Searched")
+    }
+    res.send(`<h1>These are the results of @${q} </h1>`)
+})
