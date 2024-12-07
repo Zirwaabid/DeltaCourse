c8f8e644d690ec73e772ddd7e2a1f95840c92dce
Zirwa Abid <zirwaabid@gmail.com>
2024-12-07 22:52:37 +0500

    delta

--- Backend/EJS/InstaMiniProject/index.js
+++ Backend/EJS/InstaMiniProject/index.js
@@ -6,10 +6,12 @@ const path = require("path");
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "/views"))
 
-app.get("/ig", (req, res) => {
-res.render("insta.ejs")
-})
+app.get("/ig/:username", (req, res) => {
+    const instaData = require("./data.json")
+    console.log(instaData)
+    res.render("insta.ejs")
+});
 
 app.listen(port, () => {
     console.log(`app is listening on port ${port}`)
-})
+});
