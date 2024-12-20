23d271bcc4a766b0204a2f3d410cdeb1dbdd3218
Zirwa Abid <zirwaabid@gmail.com>
2024-12-20 12:46:57 +0500

    delta

--- Backend/Day 34/Project/index.js
+++ Backend/Day 34/Project/index.js
@@ -1,4 +1,3 @@
-const exp = require("constants");
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -9,7 +8,39 @@ app.use(express.json());
 
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "/views"))
+app.use(express.static(path.join(__dirname, "public")))
 
 app.listen(port, () => {
     console.log("app is listening on port");
+})
+
+let posts = [
+    {
+        username: "Zirwa Abid",
+        content: "I am an inspiring Web Developer"
+    },
+    {
+        username: "Anas Rafiq",
+        content: "Hardwork is very important for success"
+    },
+    {
+        username: "Alkaram",
+        content: "Winter sale is live now so hurry up and fill your carts"
+    }
+]
+
+// to get data for all posts
+app.get("/posts", (req, res) => {
+    res.render("index.ejs", { posts })
+});
+
+//to create new post
+app.get("/posts/new", (req, res) => {
+    res.render("new.ejs");
+});
+
+app.post("/posts", (req, res) => {
+    let { username, content } = req.body;
+    posts.push({ username, content })
+    res.redirect("/posts")
 })
