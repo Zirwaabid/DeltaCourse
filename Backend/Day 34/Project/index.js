e12bf276ac896351de0259dc639448baa4ad1d31
Zirwa Abid <zirwaabid@gmail.com>
2024-12-21 12:39:30 +0500

    delta

--- Backend/Day 34/Project/index.js
+++ Backend/Day 34/Project/index.js
@@ -10,20 +10,25 @@ app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "/views"))
 app.use(express.static(path.join(__dirname, "public")))
 
+const { v4: uuidv4 } = require("uuid")
+
 app.listen(port, () => {
     console.log("app is listening on port");
 })
 
 let posts = [
     {
+        id: uuidv4(),
         username: "Zirwa Abid",
         content: "I am an inspiring Web Developer"
     },
     {
+        id: uuidv4(),
         username: "Anas Rafiq",
         content: "Hardwork is very important for success"
     },
     {
+        id: uuidv4(),
         username: "Alkaram",
         content: "Winter sale is live now so hurry up and fill your carts"
     }
@@ -41,6 +46,14 @@ app.get("/posts/new", (req, res) => {
 
 app.post("/posts", (req, res) => {
     let { username, content } = req.body;
-    posts.push({ username, content })
+    let id = uuidv4();
+    posts.push({ username, content, id })
     res.redirect("/posts")
+})
+
+// to get single post on the basis of id
+app.get("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => id === p.id)
+    res.render("show.ejs", { post })
 })
