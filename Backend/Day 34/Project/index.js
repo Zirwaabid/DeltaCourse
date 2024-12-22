70abd47a57f3c633e3a9f483bdf9d0699fdb0008
Zirwa Abid <zirwaabid@gmail.com>
2024-12-22 12:51:08 +0500

    delta

--- Backend/Day 34/Project/index.js
+++ Backend/Day 34/Project/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+var methodOverride = require('method-override')
 let port = 8080;
 
+app.use(methodOverride('_method'))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -61,6 +63,22 @@ app.get("/posts/:id", (req, res) => {
 // to update/edit post
 app.patch("/posts/:id", (req, res) => {
     let newContent = req.body.content;
-    console.log(newContent)
-    res.send("working")
+    let { id } = req.params;
+    let post = posts.find((p) => id === p.id);
+    post.content = newContent
+    res.redirect("/posts")
+});
+
+// create form for update
+app.get("/posts/:id/:edit", (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => id === p.id)
+    res.render("edit.ejs", { post });
+})
+
+// delete post
+app.delete("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    posts = posts.filter((p) => id !== p.id)
+    res.redirect("/posts")
 })
