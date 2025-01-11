f769775820b6bb9d59736f31c692e62809390b1c
Zirwa Abid <zirwaabid@gmail.com>
2025-01-11 15:25:48 +0500

    delta

--- Backend/Day 38/Mongosh/index.sh
+++ Backend/Day 38/Mongosh/index.sh
@@ -1,10 +1,10 @@
-# # mongosh commands'
+# mongosh commands'
 
-# # create dataBase 
-# use college
-# # insert one document in collection
-#  db.student.insertOne({name:"zirwa",age:20,marks:99})
-# # insert many document in collection
-# db.student.insertMany([{name:"donald",age:30,marks:45},{name:"defne",city:"multan",age:23}])
-# # to find document 
-# db.student.find()
+# create dataBase 
+use college
+# insert one document in collection
+ db.student.insertOne({name:"zirwa",age:20,marks:99})
+# insert many document in collection
+db.student.insertMany([{name:"donald",age:30,marks:45},{name:"defne",city:"multan",age:23}])
+# to find document 
+db.student.find()
