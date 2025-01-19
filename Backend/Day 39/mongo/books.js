553cd36a7cc41e7e56a6cdddec88610775dbb504
Zirwa Abid <zirwaabid@gmail.com>
2025-01-19 17:31:56 +0500

    delta

--- Backend/Day 39/mongo/books.js
+++ Backend/Day 39/mongo/books.js
@@ -21,12 +21,37 @@ const bookSchema = new mongoose.Schema({
     },
     price: {
         type: Number,
+        min: [1, "please enter a valid price"]
+    },
+    genre: {
+        type: Number,
+        default: 0,
+    },
+    category: {
+        type: String,
+        enum: ["fiction", "non-fiction"]
     }
-})
+});
 
 
 //create collection
 const Book = mongoose.model("Book", bookSchema);
 
 // insert data 
-const book1=new B
+// const book1 = new Book({
+//     title: "The Loneliness",
+//     author: "Zirwa",
+//     price: 500,
+
+// });
+// book1.save()
+//     .then((res) => {
+//         console.log(res);
+//     });
+
+Book.findByIdAndUpdate('678cec36552deafc0ef9edaa', { price: -500 }, { runValidators: true })
+    .then((res) => {
+        console.log(res)
+    }).catch((err) =>
+        console.log(err.errors.price.properties.message)
+);
