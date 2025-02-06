a08512bc02dd74381761c6c2980e488ba5d4b71e
Zirwa Abid <zirwaabid112@gmail.com>
2025-02-06 11:56:31 +0500

    delta

---
+++ Backend/Day 43/Middlewares/app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+
+// create random middleware
+app.use((req, res, next) => {
+    console.log("hi i am a middleware");
+    next();
+});
+
+// create our logger like morgan(npm package)
+app.use((req, res, next) => {
+    req.time = new Date(Date.now()).toString(); // create our own parameter
+    console.log(req.method, req.path, req.hostname, req.time);
+    next();
+});
+
+// activity 
+const checkToken = (req, res, next) => {
+    let { token } = req.query;
+    if (token === "accesstoken") {
+        next()
+    };
+    res.send("ACCESS DENIED");
+};
+
+app.get("/api", checkToken, (req, res) => {
+    res.send("data");
+});
+
+app.use("/random", (req, res, next) => {
+    console.log("I am only for random");
+    next();
+});
+
+app.get("/", (req, res) => {
+    res.send("Hi i am a root");
+});
+app.get("/random", (req, res) => {
+    res.send("Hi i am random");
+})
+
+app.listen("8080", () => {
+    console.log("app is listening on port 8080");
+})
