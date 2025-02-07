0c120f30b859038ab9f00f71eba03738dcb274ff
Zirwa Abid <zirwaabid112@gmail.com>
2025-02-07 19:12:25 +0500

    delta

--- Backend/Day 40/MongoWithExpress/index.js
+++ Backend/Day 40/MongoWithExpress/index.js
@@ -24,7 +24,7 @@ main().then(
     .catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+    await mongoose.connect('mongodb://127.0.0.1:27017/fakewhatsapp');
 }
 
 // create paths 
--- Backend/Day 40/MongoWithExpress/init.js
+++ Backend/Day 40/MongoWithExpress/init.js
@@ -9,7 +9,7 @@ main().then(
     .catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+    await mongoose.connect('mongodb://127.0.0.1:27017/fakewhatsapp');
 }
 
 //insert into model chat
