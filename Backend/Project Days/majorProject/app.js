d9db95b5fd2b3d12d9abb7577977fa51d8d6b4a5
Zirwa Abid <zirwaabid112@gmail.com>
2025-03-10 20:44:53 +0500

    delta

--- Backend/Project Days/majorProject/app.js
+++ Backend/Project Days/majorProject/app.js
@@ -121,6 +121,15 @@ app.post("/listings/:id/reviews", validateReview, wrapAsync(async (req, res) => 
     res.redirect(`/listings/${listing._id}`)
 }));
 
+// delete route for reviews 
+app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async (req, res) => {
+    let { id, reviewId } = req.params;
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } })
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/listings/${id}`)
+}));
+
+
 app.all("*", (req, res, next) => {
     next(new expressError(404, "page not found!"));
 })
