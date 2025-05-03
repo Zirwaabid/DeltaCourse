9ba4f66282276ace560e3023a53c78d15378890d
Zirwa Abid <zirwaabid112@gmail.com>
2025-05-03 13:14:22 +0500

    react

--- React/firstLecture/activity/amazon-card/src/ItemsDescription.jsx
+++
@@ -1,9 +0,0 @@
-import "./ItemsDescription.css"
-export default function ItemsDescription({title,features}) {
-    return(
-        <div className="ItemsDescription">
-            <h3>{title}</h3>
-            <p>{features}</p>
-        </div>
-    )
-};
--- React/firstLecture/activity/amazon-card/src/items.jsx
+++
@@ -1,8 +0,0 @@
-import ItemsDescription from "./ItemsDescription"
-export default function Items() {
-    return (
-        <div className="Items">
-            <ItemsDescription title={"Logitech MX Master"} features={"8000 DPI"}/>
-        </div>
-    )
-}
