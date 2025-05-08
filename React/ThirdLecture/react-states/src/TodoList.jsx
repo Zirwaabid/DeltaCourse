2d608449c40fa5bedfdf1deb131cc13cc34cee84
Zirwa Abid <zirwaabid112@gmail.com>
2025-05-08 21:06:21 +0500

    react

--- React/ThirdLecture/react-states/src/TodoList.jsx
+++ React/ThirdLecture/react-states/src/TodoList.jsx
@@ -5,12 +5,28 @@ export default function () {
     const [todos, setTodos] = useState([{ task: "sample task", id: uuidv4() }]);
     const [newTodo, setNewTodo] = useState([""]);
     const addNewTodo = () => {
-        setTodos([...todos, { task: newTodo, id: uuidv4() }]);
+        setTodos((prevTodos) => {
+            return [...prevTodos, { task: newTodo, id: uuidv4() }]
+        });
         setNewTodo("");
     }
     const updateTodoValue = (event) => {
         setNewTodo(event.target.value);
     }
+
+    const deleteTodo = (id) => {
+        setTodos((prevTodos) => todos.filter((prevTodos) => prevTodos.id != id));
+    }
+    const uppercaseAll = () => {
+        setTodos((todos) =>
+            todos.map((todo) => {
+                return {
+                    ...todo,
+                    task: todo.task.toUpperCase()
+                };
+            })
+        );
+    };
     return <>
         <input value={newTodo} onChange={updateTodoValue} placeholder="Write Task To Add" />
         <br /><br />
@@ -21,10 +37,14 @@ export default function () {
             <h3>Todo List</h3>
             <ul>
                 {todos.map((todo) => (
-                    <li key={todo.id}>{todo.task}</li>
+                    <li key={todo.id}>
+                        <span>{todo.task}</span>
+                        &nbsp;&nbsp;&nbsp;
+                        <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+                    </li>
                 ))}
             </ul>
-
+            <button onClick={uppercaseAll}>Uppercase All</button>
         </div>
     </>
 }
