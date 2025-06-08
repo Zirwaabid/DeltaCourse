3b1309ee9088af1100f767f7c26dbdf80fa3679d
Zirwa Abid <zirwaabid112@gmail.com>
2025-06-08 12:55:39 +0500

    react mini project

---
+++ React/MaterialUi/mini-project-react/src/App.jsx
@@ -0,0 +1,13 @@
+// import './App.css'
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+function App() {
+ 
+ return (
+  <>
+  
+  </>
+  )
+}
+
+export default App
